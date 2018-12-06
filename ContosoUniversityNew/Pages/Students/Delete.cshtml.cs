d6acb3ebee4c0668293886e20a106219bdabd886
Armen Armenakyan <amosh.armenakyan@mail.ru>
2018-12-06 16:41:04 +0400

    First Commit

---
+++ ContosoUniversityNew/Pages/Students/Delete.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversityNew.Pages.Students
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public DeleteModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+        public string ErrorMessage { get; set; }
+
+    }
+}
